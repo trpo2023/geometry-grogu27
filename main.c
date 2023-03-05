315e93cf481aebfe39458a8b64c7d479ab37136b
Soskin Andrey <arthas@-.localdomain>
2023-03-05 21:58:03 +0700

    fixed error output3

--- main.c
+++ main.c
@@ -2,9 +2,6 @@
 #include <locale.h>
 #include <stdio.h>
 #include <string.h>
-<<<<<<< HEAD
-#include <locale.h>
-#include <ctype.h>
 void error_isObject(int index)
 {
 	printf("Ошибка на элементе %d: Неправильный ввод названия объекта\n", index);
@@ -97,71 +94,11 @@ void isObject(char *str)
 	if (res)
 		error_isObject(target);
 	
-=======
-int isArguments(char* str)
-{
-    int res = 1;
-    int a;
-    for (int i = 7; str[i] != ')'; i++) {
-        if ((str[i] >= 48 && str[i] <= 57) || str[i] == '.' || str[i] == ',' || str[i] == ' ')
-            
-            res = 0;
-        else {
-            res = 1;
-            a = i;
-            break;
-        }
-    }
-    
-    return res;
-}
 
-int isEnd(char* str)
-{
-    int res = 1;
-    int a;
-    int index = 0;
-    int endSymbol = strlen(str) - 2;
-    for (int i = 0; i < strlen(str); i++) {
-        if (str[i] == ')') {
-            index = i;
-            break;
-        }
-    }
-    if (index == endSymbol) {
-        res = 0;
-    }
-    return res;
-}
-
-int isObject(char* str)
-{
-    int res = 1;
-    int a;
-    char rec[100];
-    for (int i = 0; i < strlen(str); i++) {
-        if (str[i] != '(')
-            rec[i] = str[i];
-        else
-            break;
-    }
-    char figure[] = "circle";
-
-    for (int i = 0; i < strlen(rec); i++) {
-        rec[i] = tolower(rec[i]);
-    }
-
-    if (strcmp(rec, figure) == 0) {
-        res = 0;
-    }
-    
-    return res;
->>>>>>> 347fb02d66467ced6cf3eaae4f86adeca58658d6
-}
 
 int main(int argc, char** argv)
 {
-<<<<<<< HEAD
+
 	setlocale(LC_ALL, " ");
 	FILE *file;
 	file = fopen("data.txt", "r");
@@ -174,23 +111,3 @@ int main(int argc, char** argv)
 	fclose(file);
 	return 0;
 }
-=======
-    setlocale(LC_ALL, " ");
-    FILE* file;
-			
-    file = fopen("data.txt", "r");
-    char str[100];
-    fgets(str, 99, file);
-    if (isObject(str))
-        printf("%s\nНеправильный ввод названия объекта\n", str);
-    else if (isArguments(str))
-        printf("%s\nНеправильно введены данные объекта\n", str);
-    else if (isEnd(str))
-        printf("%s\nНеправильный завершающий символ\n",str);
-               
-    else
-        printf("%s\n", str);
-    fclose(file);
-    return 0;
-}
->>>>>>> 347fb02d66467ced6cf3eaae4f86adeca58658d6
