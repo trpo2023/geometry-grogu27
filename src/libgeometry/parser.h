b4edd0ce9acb0d95be0ea0bf356e5e14c5f55b47
Соскин Андрей Сергеевич <iv222s22@lwp10.csc.local>
2023-04-25 10:09:28 +0700

    use clang-format 2

--- src/libgeometry/parser.h
+++ src/libgeometry/parser.h
@@ -1,15 +1,15 @@
 #include <ctype.h>
 #include <locale.h>
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
-double calculate_perimetr_circle(char *str);
-double calculate_area_circle(char *str);
-int area_calculation(char *str);
+double calculate_perimetr_circle(char* str);
+double calculate_area_circle(char* str);
+int area_calculation(char* str);
 void error_isObject(int index);
 void error_isArguments(int index);
 void error_isEnd(int index);
-int check_argument(char *str);
-void check_end(char *str);
-void check_name_Object(char *str);
+int check_argument(char* str);
+void check_end(char* str);
+void check_name_Object(char* str);
